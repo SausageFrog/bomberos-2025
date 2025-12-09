856120431575f0dd54a00d5a05f00ade59def41c
Nico Quevedo <ee.nicoo@gmail.com>
2025-12-09 01:50:43 -0300

    Fix querys, Fix radio.html, Arreglos en index

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const imageURL = [
+    './images/BomberosHero1.jpg',
+    './images/BomberosHero2.jpg'
+];
+
+let currentIndex = 0;
+const heroElement = document.querySelector('.hero');
+
+function changeBackground() {
+    const gradient = 'linear-gradient(rgba(0, 0, 0, 0.6)'
+
+    heroElement.style.backgroundImage = "${gradient}, url('${imageURL}')";
+    currentIndex++;
+
+    if (currentIndex >= imageURL.length) {
+        currentIndex = 0;
+    }
+}
+
+changeBackground();
+
+const intervalTime = 5000;
+setInterval(changeBackground, intervalTime);
+
 const headerSection = document.querySelector('.header-section');
 const toggleButton = document.querySelector('.header-menu-toggle');
 const navLinks = document.getElementById('main-nav');
